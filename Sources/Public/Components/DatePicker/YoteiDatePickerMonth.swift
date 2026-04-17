1c0ab0d273a4cb396a5ffdefd071c3589dd824ff
Mikalai Zmachynski <nikolai.zmachinsky@gemspace.com>
2026-04-17 19:44:12 +0200

    refactored DateTabView

--- Sources/Public/Components/DatePicker/YoteiDatePickerMonth.swift
+++ Sources/Public/Components/DatePicker/YoteiDatePickerMonth.swift
@@ -33,6 +33,12 @@ public struct YoteiDatePickerMonth<ViewFactory: YoteiDatePickerFactoryProtocol>:
     }
 
     public var body: some View {
+        let minDate = minDate.flatMap {
+            calendar.startOfDay(for: $0)
+        }
+        let maxDate = maxDate.flatMap {
+            calendar.startOfDay(for: $0)
+        }
         let monthInterval = calendar.dateInterval(of: .month, for: dateInMonth)!
         let startDate = calendar.dateInterval(
             of: .weekOfMonth,
@@ -43,79 +49,50 @@ public struct YoteiDatePickerMonth<ViewFactory: YoteiDatePickerFactoryProtocol>:
             in: .month,
             for: dateInMonth
         )!.count
-
-        MainView(
-            selectedDate: $selectedDate,
-            minDate: minDate.flatMap {
-                calendar.startOfDay(for: $0)
-            },
-            maxDate: maxDate.flatMap {
-                calendar.startOfDay(for: $0)
-            },
-            daysSequence: YoteiDaysSequence(
-                startDate: startDate,
-                days: numberOfWeeks * Constants.numberOfDaysPerWeek,
-                calendar: calendar
-            ),
-            monthInterval: monthInterval,
-            numberOfWeeks: numberOfWeeks,
-            viewFactory: viewFactory
+        let daysSequence = YoteiDaysSequence(
+            startDate: startDate,
+            days: numberOfWeeks * Constants.numberOfDaysPerWeek,
+            calendar: calendar
         )
-    }
-}
 
-private extension YoteiDatePickerMonth {
-    struct MainView: View {
-        @Environment(\.calendar) private var calendar
-
-        @Binding var selectedDate: Date
-        let minDate: Date?
-        let maxDate: Date?
-        let daysSequence: YoteiDaysSequence
-        let monthInterval: DateInterval
-        let numberOfWeeks: Int
-        let viewFactory: ViewFactory
-
-        var body: some View {
-            let todayDate = Date.now
-            Grid(horizontalSpacing: 0, verticalSpacing: viewFactory.weekInteritemVerticalSpacing()) {
-                ForEach(0 ..< numberOfWeeks, id: \.self) { row in
-                    GridRow {
-                        ForEach(0 ..< Constants.numberOfDaysPerWeek, id: \.self) { col in
-                            let date = daysSequence[row * Constants.numberOfDaysPerWeek + col]
-                            if monthInterval.contains(date), monthInterval.end != date {
-                                let isEnabled = (minDate.flatMap { $0 <= date } ?? true)
-                                    && (maxDate.flatMap { $0 >= date } ?? true)
-                                Button(action: {
-                                    let selectedDateComponents = calendar.dateComponents(
-                                        [.hour, .minute, .second],
-                                        from: selectedDate
-                                    )
-                                    let newDate = calendar.date(
-                                        bySettingHour: selectedDateComponents.hour!,
-                                        minute: selectedDateComponents.minute!,
-                                        second: selectedDateComponents.second!,
-                                        of: date
-                                    )!
-                                    selectedDate = newDate
-                                }, label: {
-                                    viewFactory.dayCellView(
-                                        date: date,
-                                        todayDate: todayDate,
-                                        focusedDate: selectedDate,
-                                        isEnabled: isEnabled
-                                    )
-                                })
-                                .disabled(!isEnabled)
-                            } else {
-                                Color.clear.frame(height: 1)
-                            }
+        let todayDate = Date.now
+        Grid(horizontalSpacing: 0, verticalSpacing: viewFactory.weekInteritemVerticalSpacing()) {
+            ForEach(0 ..< numberOfWeeks, id: \.self) { row in
+                GridRow {
+                    ForEach(0 ..< Constants.numberOfDaysPerWeek, id: \.self) { col in
+                        let date = daysSequence[row * Constants.numberOfDaysPerWeek + col]
+                        if monthInterval.contains(date), monthInterval.end != date {
+                            let isEnabled = (minDate.flatMap { $0 <= date } ?? true)
+                                && (maxDate.flatMap { $0 >= date } ?? true)
+                            Button(action: {
+                                let selectedDateComponents = calendar.dateComponents(
+                                    [.hour, .minute, .second],
+                                    from: selectedDate
+                                )
+                                let newDate = calendar.date(
+                                    bySettingHour: selectedDateComponents.hour!,
+                                    minute: selectedDateComponents.minute!,
+                                    second: selectedDateComponents.second!,
+                                    of: date
+                                )!
+                                selectedDate = newDate
+                            }, label: {
+                                viewFactory.dayCellView(
+                                    date: date,
+                                    todayDate: todayDate,
+                                    focusedDate: selectedDate,
+                                    isEnabled: isEnabled
+                                )
+                            })
+                            .disabled(!isEnabled)
+                        } else {
+                            Color.clear.frame(height: 1)
                         }
                     }
                 }
             }
-            .buttonStyle(.plain)
-            .frame(maxHeight: .infinity, alignment: .center)
         }
+        .buttonStyle(.plain)
+        .frame(maxHeight: .infinity, alignment: .center)
     }
 }
